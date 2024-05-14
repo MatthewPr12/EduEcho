fb6b79167de09ef865a785ce787376309cab6d56
yakorch <korchyar19@gmail.com>
2024-05-14 07:00:21 +0300

    fixed a typo

--- consul_service/consul_utils.py
+++ consul_service/consul_utils.py
@@ -1,23 +1,17 @@
 import consul
 import os
 
+
 def register_service(service_name, service_id, port):
-    c = consul.Consul(host=os.getenv("CONSUL_PORT"), port=os.getenv("CONSUL_PORT"))
-    c.agent.service.register(
-        name=service_name,
-        service_id=service_id,
-        address='localhost',
-        port=port,
-        tags=['microservice']
-    )
+    c = consul.Consul(host=os.getenv("CONSUL_HOST"), port=os.getenv("CONSUL_PORT"))
+    c.agent.service.register(name=service_name, service_id=service_id, address="localhost", port=port, tags=["microservice"])
 
 
 def get_service_urls(service_name):
     c = consul.Consul()
     _, services = c.health.service(service_name, passing=True)
     if services:
-        return [f"http://{service['Service']['Address']}:{service['Service']['Port']}"
-                for service in services if service['Service']]
+        return [f"http://{service['Service']['Address']}:{service['Service']['Port']}" for service in services if service["Service"]]
 
     raise Exception("No available services found")
 
@@ -31,5 +25,5 @@ def get_config(key):
     c = consul.Consul()
     _, data = c.kv.get(key)
     if data:
-        return data['Value'].decode('utf-8')
+        return data["Value"].decode("utf-8")
     return None
