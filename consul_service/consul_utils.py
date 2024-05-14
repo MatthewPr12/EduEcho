4b5fd7f9bfce3cf42c554afe3fcad29493559f8f
yakorch <korchyar19@gmail.com>
2024-05-14 07:46:35 +0300

    correct consul connection

--- consul_service/consul_utils.py
+++ consul_service/consul_utils.py
@@ -1,14 +1,15 @@
 import consul
 import os
+import socket
+
+c = consul.Consul(host=os.getenv("CONSUL_HOST"), port=os.getenv("CONSUL_PORT"))
 
 
 def register_service(service_name, service_id, port):
-    c = consul.Consul(host=os.getenv("CONSUL_HOST"), port=os.getenv("CONSUL_PORT"))
-    c.agent.service.register(name=service_name, service_id=service_id, address="localhost", port=port, tags=["microservice"])
+    c.agent.service.register(name=service_name, service_id=service_id, address=socket.gethostname(), port=port, tags=["microservice"])
 
 
 def get_service_urls(service_name):
-    c = consul.Consul()
     _, services = c.health.service(service_name, passing=True)
     if services:
         return [f"http://{service['Service']['Address']}:{service['Service']['Port']}" for service in services if service["Service"]]
@@ -17,12 +18,10 @@ def get_service_urls(service_name):
 
 
 def set_config(key, value):
-    c = consul.Consul()
     c.kv.put(key, value)
 
 
 def get_config(key):
-    c = consul.Consul()
     _, data = c.kv.get(key)
     if data:
         return data["Value"].decode("utf-8")
